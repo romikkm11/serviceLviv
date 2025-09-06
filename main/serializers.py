14c94c95a2871f3fb9a4d65f1df22ba57b1f2154
romikkm11 <romikkm11@gmail.com>
2025-09-06 21:18:43 +0300

    add logo parser

--- main/serializers.py
+++ main/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Service
 
 class ServiceSerializer(serializers.ModelSerializer):
+    company_name = serializers.CharField(source="company.name", read_only=True)
+    company_logo_url = serializers.CharField(source="company.logo_url", read_only=True)
+    company_url = serializers.CharField(source="company.company_url", read_only=True)
+    service_type_name = serializers.CharField(source="service_type.name", read_only=True)
+    service_general_name = serializers.CharField(source="service_general_name.name", read_only=True)
+    service_general_name_id = serializers.IntegerField(source="service_general_name.id", read_only=True)
     class Meta:
         model = Service
         fields = '__all__'
