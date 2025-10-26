6914c97c72602a3c337ff95191a30e6b17962724
romikkm11 <romikkm11@gmail.com>
2025-10-26 11:28:34 +0200

    changes

--- main/geocoder/geocoder.py
+++ main/geocoder/geocoder.py
@@ -5,7 +5,8 @@ django.setup()
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'config')) ###Шлях до API геокодера
 import config ###Імпорт файлу з API геокодера
 from .decorators import geocoder_api_limit
-
+import logging
+logger = logging.getLogger(__name__)
 @geocoder_api_limit
 def geocode_address(address_text):
     url = 'https://geocode.search.hereapi.com/v1/geocode'
@@ -17,5 +18,17 @@ def geocode_address(address_text):
     }
 
     response = requests.get(url, params=params)
-    position = response.json()['items'][0]['position']
-    return position['lat'], position['lng']
+    try:
+        1 / 0  # штучна помилка
+    except Exception:
+        logger.exception("Тест логування")
+    try:
+        data = response.json()
+        logger.debug("HERE API response for '%s': %s", address_text, data)
+        position = data['items'][0]['position']
+        return position['lat'], position['lng']
+    except KeyError:
+        logger.error("HERE API не повернув 'items' для '%s'. Повна відповідь: %s", address_text, response.text)
+        raise
+    # position = response.json()['items'][0]['position']
+    # return position['lat'], position['lng']
